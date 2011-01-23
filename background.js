19b2d0049a6cd8671bf3101f6b1184b7665e1a2e
Michael Ficarra <github@michael.ficarra.me>
2011-01-23 04:02:32 -0500

    implemented automatic tracker retrieval from torrentz and bitsnoop

--- background.js
+++ background.js
@@ -41,12 +41,75 @@
 		refreshIcon(tab);
 	});
 
-	var clicked = function(){
+
+	var getTrackers = function(info_hash, callback){
+		info_hash = info_hash.toLowerCase();
+		var trackers = [], running = 0,
+			finish = function(){
+				if(--running < 1) callback(trackers);
+			};
+
+		var xhrTorrentz = new XMLHttpRequest(),
+			torrentzDomains = [
+				'https://torrentz.eu',
+				'https://torrentz.me',
+				'http://torrentz.eu',
+				'http://torrentz.me',
+				'https://torrentz.com',
+				'http://torrentz.com'
+			]
+		var tryTorrentz = function(){
+			var url = torrentzDomains.shift();
+			if(!url) return finish();
+			xhrTorrentz.onreadystatechange = function(){
+				if(xhrTorrentz.readyState!=4) return;
+				if(xhrTorrentz.status != 200) tryTorrentz();
+				var text = xhrTorrentz.responseText;
+				if(!text) tryTorrentz();
+				var list = text.match(/(.{0,3}ps?:\/\/.+\/announce)/gi);
+				if(list.length < 1) tryTorrentz();
+				var i=0, l=list.length;
+				for(; i<l; ++i)
+					if(0 > trackers.indexOf(list[i]))
+						trackers.push(list[i]);
+				finish();
+			};
+			xhrTorrentz.open("GET", url + '/announce_' + info_hash, true);
+			xhrTorrentz.send();
+		}
+
+		var xhrBitsnoop = new XMLHttpRequest();
+		var url = "http://bitsnoop.com/api/trackers.php?json=1&hash=" + info_hash;
+		xhrBitsnoop.onreadystatechange = function(){
+			if(xhrBitsnoop.readyState != 4) return;
+			if(xhrBitsnoop.status != 200) return finish();
+			var text = xhrBitsnoop.responseText;
+			if(!text || text == "NOTFOUND" || text == "ERROR") return finish();
+			var json = JSON.parse(text);
+			if(!json || typeof json.length != "number" || json.length < 1) return finish();
+			var i=0, l=json.length;
+			for(; i<l; ++i)
+				if(0 > trackers.indexOf(json[i]["ANNOUNCE"]))
+					trackers.push(json[i]["ANNOUNCE"]);
+			return finish();
+		};
+		xhrBitsnoop.open("GET", url, true);
+
+		++running;
+		xhrBitsnoop.send();
+
+		++running;
+		tryTorrentz();
+	};
+
+	chrome.pageAction.onClicked.addListener(function(){
 		log('clicked');
 		if(currentTab < 0) return;
 		chrome.tabs.sendRequest(currentTab, {reqtype:'info_hash'}, function(info_hash){
-			log('retrieved info_hash',info_hash);
+			log('determined info_hash',info_hash);
+			getTrackers(info_hash,function(trackers){
+				log('retrieved trackers',trackers);
+			});
 		});
-	};
-	chrome.pageAction.onClicked.addListener(clicked);
+	});
 })()
